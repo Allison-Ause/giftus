0ea5ec3b1c7ec64d7a03252d2c222634ed29d8d9
Allison Ause <allison.ause@gmail.com>
2022-11-14 11:55:22 -0800

    created form and clickHandler for signing up new user.

--- client/app.tsx
+++ client/app.tsx
@@ -1,29 +1,34 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './reset.css';
-import './light-theme.css'
-import './dark-theme.css'
+import './light-theme.css';
+import './dark-theme.css';
 import './global.css';
-import fooListFn from './components/foo-list'
-import catListFn from './components/cat-list'
-import dashboardFn from './components/dashboard'
-import Layout from './layout'
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import fooListFn from './components/foo-list';
+import catListFn from './components/cat-list';
+import dashboardFn from './components/dashboard';
+import Layout from './layout';
+import {
+  Route,
+  BrowserRouter as Router,
+  Routes,
+} from 'react-router-dom';
+import AuthPage from './components/AuthPage';
 
-const FooList = fooListFn()
-const CatList = catListFn()
-const Dashboard = dashboardFn()
-const container = document.getElementById('app') || document.createElement('div')
-container.id = 'app'
+const CatList = catListFn();
+const Dashboard = dashboardFn();
+const container =
+  document.getElementById('app') || document.createElement('div');
+container.id = 'app';
 const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Router>
       <Routes>
-        <Route element={ <Layout /> }>
-          <Route index element={ <Dashboard /> } />
-          <Route path="foos" element={ <FooList /> } />
-          <Route path="cats" element={ <CatList /> } />
+        <Route element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="/auth/:type" element={<AuthPage />} />
+          <Route path="cats" element={<CatList />} />
         </Route>
       </Routes>
     </Router>
