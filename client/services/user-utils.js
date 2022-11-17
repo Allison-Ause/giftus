aefc9d6e91ad9380b0c2eda1fd960a2b8e0c3731
Allison Ause <allison.ause@gmail.com>
2022-11-17 14:20:37 -0800

    added fetch route for front end user sign in

--- client/services/user-utils.js
+++ client/services/user-utils.js
@@ -15,6 +15,7 @@ export async function signUpUser(userInfo) {
   });
   const data = await res.json();
   if (res.ok) {
+    location.replace('/');
     console.log('data', data);
   } else {
     console.error(data.message);
@@ -23,4 +24,20 @@ export async function signUpUser(userInfo) {
 
 export async function signInUser(userInfo) {
   console.log('userInfo in fetch', userInfo);
+  const res = await fetch(`${BASE_URL}/users/sessions`, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    credentials: 'include',
+    body: JSON.stringify(userInfo),
+  });
+  const data = await res.json();
+
+  if (res.ok) {
+    location.replace('/');
+  } else {
+    console.error(data.message);
+  }
 }
