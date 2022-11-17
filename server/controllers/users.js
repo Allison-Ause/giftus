7d4270ae9b1ba4fbef63f3f2126857ca7ed35cb1
Allison Ause <allison.ause@gmail.com>
2022-11-17 14:26:23 -0800

    added controller post route for user sign in

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -3,21 +3,40 @@ import UserService from '../services/UserService.js';
 
 const ONE_DAY_IN_MS = 1000 * 60 * 60 * 24;
 
-export default Router().post('/', async (req, res, next) => {
-  try {
-    console.log('controller req.body', req.body);
-    const token = await UserService.signUp(req.body);
-    console.log('controller: token', token);
-    res
-      .cookie(process.env.COOKIE_NAME, token, {
-        httpOnly: true,
-        secure: process.env.SECURE_COOKIES === 'true',
-        sameSite:
-          process.env.SECURE_COOKIES === 'true' ? 'none' : 'strict',
-        maxAge: ONE_DAY_IN_MS,
-      })
-      .json({ message: 'Successfully logged in with new account!' });
-  } catch (e) {
-    next(e);
-  }
-});
+export default Router()
+  .post('/', async (req, res, next) => {
+    try {
+      console.log('controller req.body', req.body);
+      const token = await UserService.signUp(req.body);
+      console.log('controller: token', token);
+      res
+        .cookie(process.env.COOKIE_NAME, token, {
+          httpOnly: true,
+          secure: process.env.SECURE_COOKIES === 'true',
+          sameSite:
+            process.env.SECURE_COOKIES === 'true' ? 'none' : 'strict',
+          maxAge: ONE_DAY_IN_MS,
+        })
+        .json({
+          message: 'Successfully logged in with new account!',
+        });
+    } catch (e) {
+      next(e);
+    }
+  })
+  .post('/sessions', async (req, res, next) => {
+    try {
+      const sessionToken = await UserService.signIn(req.body);
+      res
+        .cookie(process.env.COOKIE_NAME, sessionToken, {
+          httpOnly: true,
+          secure: process.env.SECURE_COOKIES === 'true',
+          sameSite:
+            process.env.SECURE_COOKIES === 'true' ? 'none' : 'strict',
+          maxAge: ONE_DAY_IN_MS,
+        })
+        .json({ message: 'Successfully signed in!' });
+    } catch (e) {
+      next(e);
+    }
+  });
