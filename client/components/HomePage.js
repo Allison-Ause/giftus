c7de44582851a56a0e1c3f1a5cfb67bead859157
Allison Ause <allison.ause@gmail.com>
2022-11-27 13:54:28 -0800

    created sub component for gift display list

--- client/components/HomePage.js
+++ client/components/HomePage.js
@@ -1,4 +1,5 @@
 import useGifts from '../hooks/useGifts.js';
+import Gift from './Gift.js';
 import NewGiftForm from './NewGiftForm.js';
 
 export default function HomePage() {
@@ -9,9 +10,11 @@ export default function HomePage() {
       <NewGiftForm />
       <div>
         <h1>This is where the data is!</h1>
-        {gifts.map((gift) => (
-          <div key={gift.id}>{gift.idea}</div>
-        ))}
+        <div>
+          {gifts.map((gift) => (
+            <Gift key={gift.id} {...gift} />
+          ))}
+        </div>
       </div>
     </>
   );
