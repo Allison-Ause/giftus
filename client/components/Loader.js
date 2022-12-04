d5547aafb4b058b853046c0118dcf9ff59b437b3
Allison Ause <allison.ause@gmail.com>
2022-12-04 14:42:10 -0800

    added loader dom component

---
+++ client/components/Loader.js
@@ -0,0 +1,10 @@
+import styles from '../global.css';
+
+export default function Loader() {
+  return (
+    <>
+      <div className={styles.ball}></div>
+      <div className={styles.shadow}></div>
+    </>
+  );
+}
