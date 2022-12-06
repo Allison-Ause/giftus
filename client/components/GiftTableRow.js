71aef2b314c66263ce7ddb1dce91ead6fe889cdd
Allison Ause <allison.ause@gmail.com>
2022-12-06 13:02:31 -0800

    added Link to gift idea on TableRow component to link to detail page

--- client/components/GiftTableRow.js
+++ client/components/GiftTableRow.js
@@ -1,9 +1,14 @@
-import { Td, Tr } from '@chakra-ui/react';
+import { Link, Td, Tr } from '@chakra-ui/react';
+import { Link as RLink } from 'react-router-dom';
 
 export default function GiftTableRow({ gift }) {
   return (
     <Tr>
-      <Td>{gift.idea}</Td>
+      <Td>
+        <Link as={RLink} to={`${gift.id}`} fontWeight="bold">
+          {gift.idea}
+        </Link>
+      </Td>
       <Td>{gift.friend.name}</Td>
       <Td>{`$${gift.price}`}</Td>
       <Td>{gift.occasion}</Td>
