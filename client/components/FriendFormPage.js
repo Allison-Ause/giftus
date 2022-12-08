71b1ad57671b816d93b015fba771f18703c3d994
Allison Ause <allison.ause@gmail.com>
2022-12-08 14:24:58 -0800

    added link to detail page from main friends display

--- client/components/FriendFormPage.js
+++ client/components/FriendFormPage.js
@@ -1,19 +1,3 @@
-import {
-  Box,
-  Button,
-  Flex,
-  FormControl,
-  FormErrorMessage,
-  FormHelperText,
-  FormLabel,
-  Input,
-  Stack,
-  Textarea,
-  useToast,
-} from '@chakra-ui/react';
-import { useState } from 'react';
-import { addFriend } from '../services/friend-utils.js';
-import styles from '../global.css';
 import Header from './Header.js';
 import Loader from './Loader.js';
 import FriendForm from './FriendForm.js';
