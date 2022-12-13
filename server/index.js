188d45a28a0579dd103566bb5c8a0ab7f331fb2f
Allison Ause <allison.ause@gmail.com>
2022-12-12 16:08:49 -0800

    successfully sent text message

--- server/index.js
+++ server/index.js
@@ -8,11 +8,16 @@
  * Any deployments will need to execute the transpiled version of this file.
  ******************************************************************************/
 
-import dotenv from 'dotenv'
-import app from './app.js'
+import dotenv from 'dotenv';
+import app from './app.js';
 
-dotenv.config()
+dotenv.config();
 
-const server = app.listen(parseInt(process.env.PORT || '7890'), () => {
-  console.log('Started server on ', server.address())
-})
+const server = app.listen(
+  parseInt(process.env.PORT || '7890'),
+  () => {
+    console.log('Started server on ', server.address());
+  }
+);
+
+// this is where my setInterval will be for running my function
---
+++ server/services/TwilioService.js
@@ -0,0 +1,36 @@
+import twilio from 'twilio';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+// const accountSid = process.env.TWILIO_ACCOUNT_SID;
+// const authToken = process.env.TWILIO_AUTH_TOKEN;
+// const client = new twilio(accountSid, authToken);
+
+const client = twilio(
+  'ACa1af9b4b71ad44e942d663be3ea9255a',
+  'a2271aa99b9a4b4eeff04418bd0b4638'
+);
+
+client.messages
+  .create({
+    body: 'Hello from Allie on Twilio',
+    from: '+19518485438',
+    to: '+19712355385',
+  })
+  .then((message) => console.log(message.sid))
+  .catch((error) => {
+    console.log(error);
+    throw error;
+  });
+
+// client.messages
+//   .create({
+//     body:
+//       'Heya, Allison! Reminder: Kylo has a 26th birthday coming up on December 13th.',
+//     to: '+19712355385',
+//     from: '+19518485438',
+//   })
+//   .then(console.log('successfully sent message'));
+
+// (message) => console.log('Firing!', message.sid)
